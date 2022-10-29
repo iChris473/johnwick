78e85582110f645cf6d64c631d58cfe938408ef6
iChris <christianjoseph473@gmail.com>
2022-10-29 14:36:42 +0100

    changed page description

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const app = express()
 require("./models/db");
 const cors = require("cors")
 
-// app.enable('trust proxy')
+app.enable('trust proxy')
 
-// app.use(function(request, response, next) {
+app.use(function(request, response, next) {
 
-//     if (process.env.NODE_ENV != 'development' && !request.secure) {
-//        return response.redirect("https://" + request.headers.host + request.url);
-//     }
+    if (process.env.NODE_ENV != 'development' && !request.secure) {
+       return response.redirect("https://" + request.headers.host + request.url);
+    }
 
-//     next();
-// })
+    next();
+})
 
 app.use(cors())
 
--- models/db.js
+++ models/db.js
@@ -7,9 +7,4 @@ mongoose.connect(process.env.MONGOURI, {
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', () => console.log("MongoDB running..."))
-
-
-// 
-
-// mongodb://ichris:uRltYApIKnACkOXK@cluster0-shard-00-00-zcbag.mongodb.net:27017,cluster0-shard-00-01-zcbag.mongodb.net:27017,cluster0-shard-00-02-xvnqv.mongodb.net:27017/zcbag?ssl=true&replicaSet=Cluster0-shard-0&authSource=test&retryWrites=true&w=majority
+db.once('open', () => console.log("MongoDB running..."));
